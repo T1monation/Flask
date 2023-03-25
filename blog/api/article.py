383c4cb56240e7dfee35394b69f8578e2d7b555c
T1monation <94930740+T1monation@users.noreply.github.com>
2023-03-25 19:58:44 +0300

    lesson_10 done! (#11)

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,11 +1,27 @@
 from flask_combo_jsonapi import ResourceList, ResourceDetail
-
+from combojsonapi.event.resource import EventsResource
 from blog.extensions import db
 from blog.models import Article
 from blog.schemas import ArticleSchema
+from urllib import request
+import requests
+
+
+class ArticleListEvent(EventsResource):
+    def event_get_count(self, **kwargs):
+        return {'count': Article.query.count()}
+
+    def event_get_api_server(self, **kwargs):
+        return {'count': requests.get('https://ipconfig.io/ip').text}
+
+
+class ArticleDeteilEvent(EventsResource):
+    def event_get_count_by_author(self, **kwargs):
+        return {'method': Article.query.filter(Article.author_id == kwargs['id']).count()}
 
 
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
@@ -14,6 +30,7 @@ class ArticleList(ResourceList):
 
 
 class ArticleDetail(ResourceDetail):
+    events = ArticleDeteilEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
